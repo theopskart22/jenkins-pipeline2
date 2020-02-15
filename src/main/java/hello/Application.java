62da12424c11f0a97fff1ec9f48f0e7ba889bec3
inderjeet709 <60532163+inderjeet709@users.noreply.github.com>
2020-02-15 18:44:27 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello team its inderjeet";
+        return "Hello team its Inderjeet";
     }
 
     public static void main(String[] args) {
